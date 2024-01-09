4ab4a1f2d7a60d689d6a661debebca47e9963c3d
aenosys <aenosys@gmail.com>
2024-01-08 22:17:03 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,22 +5,28 @@ const port = 3000;
 // Middleware to parse JSON body
 app.use(express.json());
 
-// Basic GET endpoint
+// Basic GET endpoint with a cooler response
 app.get('/', (req, res) => {
-    res.send('Welcome to the Node.js Express server!');
+    const coolResponses = [
+        "🚀 Welcome to the Node.js Express server, where coding dreams come true!",
+        "✨ You've just landed in the realm of awesome Express servers!",
+        "🌟 Hello there, explorer! Ready to embark on a Node.js adventure?"
+    ];
+    const randomIndex = Math.floor(Math.random() * coolResponses.length);
+    res.send(coolResponses[randomIndex]);
 });
 
 // GET endpoint with path parameter
 app.get('/hello/:name', (req, res) => {
     const name = req.params.name;
-    res.send(`Hello, ${name}!`);
+    res.send(`Hello, ${name}! Welcome to our cool server!`);
 });
 
 // POST endpoint to echo back data
 app.post('/echo', (req, res) => {
     const data = req.body;
     res.json({
-        message: "Received the following data:",
+        message: "Echoing back at you:",
         data
     });
 });
@@ -29,4 +35,3 @@ app.post('/echo', (req, res) => {
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
-
